030dc007d938b3632a7dc4a75c9b03696d1aa09f
cola96930 <111218629+cola96930@users.noreply.github.com>
2022-08-23 19:36:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,8 @@ def kqzl():
     conn.request('POST','/aqi/index',params,headers)
     res = conn.getresponse()
     data = res.read()
-    print(data.decode('utf-8'))
+    data = json.loads(data)
+    return data["newslist"][0]["quality"]
   
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
