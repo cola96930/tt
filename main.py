a384594483139bacaca036ae2ba11692a2a102fe
cola96930 <111218629+cola96930@users.noreply.github.com>
2022-08-23 19:54:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def tq():
     conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
     params = urllib.parse.urlencode({'key':'a59bb78a1149fb897531644c84f7d262','area':'上海'})
     headers = {'Content-type':'application/x-www-form-urlencoded'}
-    conn.request('POST','/aqi/index',params,headers)
+    conn.request('POST','/tiqi/index',params,headers)
     res = conn.getresponse()
     data = res.read()
     data = json.loads(data)
