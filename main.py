8bd06de963a9276fd213bb6a6d1fa7706ce4471d
cola96930 <111218629+cola96930@users.noreply.github.com>
2022-08-23 20:13:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def kqzl():
 
 def tq():
     conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
-   params = urllib.parse.urlencode({'key':'a59bb78a1149fb897531644c84f7d262','area':'上海'})
+    params = urllib.parse.urlencode({'key':'a59bb78a1149fb897531644c84f7d262','area':'上海市'})
         headers = {'Content-type':'application/x-www-form-urlencoded'}
         conn.request('POST','/tianqi/index',params,headers)
         res = conn.getresponse()
